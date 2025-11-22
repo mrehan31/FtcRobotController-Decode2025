4b1d17fa4084a2d979c2ed2192a5a97448b8ae4b
Mhamz <engr.mrehan@gmail.com>
2025-11-21 20:06:07 -0500

    Code updated with latest version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apex/warrior/ServoMotorConstant.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apex/warrior/ServoMotorConstant.java
@@ -6,15 +6,16 @@ package org.firstinspires.ftc.teamcode.apex.warrior;
 public class ServoMotorConstant {
 
     // Servo positions (0.0 to 1.0) Rotator Assembly
+
+
     public static final double SHOOTER_ROTATOR_SERVO_DEFAULT_POS_FAR = 0.36;  // Default position FAR from goal
     public static final double SHOOTER_ROTATOR_SERVO_DEFAULT_POS_NEAR = 0.42; // Default position NEAR to goal
     public static final double SHOOTER_ROTATOR_SERVO_START_POS_NEAR = 0.40;  // Starting position NEAR to goal
     public static final double SHOOTER_ROTATOR_SERVO_END_POS_NEAR = 0.43;  // End position from NEAR to goal
     public static final double SHOOTER_ROTATOR_SERVO_START_POS_FAR = 0.35;  // Starting position FAR from goal
     public static final double SHOOTER_ROTATOR_SERVO_END_POS_FAR = 0.39;  // End position FAR from goal
-//    public static final double FEEDER_SERVO_POS_0_DEG = 0.0;   // represents 0 degrees
-    public static final double FEEDER_SERVO_POS_0_DEG = 0.09;   // represents 0 degrees
-    public static final double FEEDER_SERVO_POS_120_DEG = 0.16; // approx 120 degrees on a 5-turn servo
-    public static final double STOPPER_SERVO_POS_INITIAL_GATE = 0.55;
+    public static final double FEEDER_SERVO_POS_0_DEG = 0.03;   // represents 0 degrees
+    public static final double FEEDER_SERVO_POS_120_DEG = 0.1; // approx 120 degrees on a 5-turn servo
+    public static final double STOPPER_SERVO_POS_INITIAL_GATE = 0.63;
     public static final double STOPPER_SERVO_POS_OPEN_GATE = 0.70;
 }
